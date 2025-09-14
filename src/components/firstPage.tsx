a94ed3ee6845816e24fecde4eb76fea4ae6058f1
vladislav <yaneznau000@gmail.com>
2025-09-14 21:43:13 +0300

    4

--- src/components/firstPage.tsx
+++ src/components/firstPage.tsx
@@ -4,7 +4,16 @@ import { FirstType } from "../types/firstType"
 const FirstPage = (props: FirstType) => {
   return (
     <>
-    
+        <header>
+            <nav>
+              <ul>
+                <li><a href="#">HOME PAGE</a></li>
+                <li><a href="#">FIRST PAGE</a></li>
+                <li><a href="#">SECOND PAGE</a></li>
+                <li><a href="#">THIRD PAGE</a></li>
+              </ul>
+            </nav>
+        </header>
     </>
     );
 };
